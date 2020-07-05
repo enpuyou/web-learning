ddf15920354d2b3be27b6bcb1c8b84085faea700
enpuyou <youe2@allegheny.edu>
2020-07-05 02:52:36 -0400

    Format indentation

--- react/todo/src/components/AddTodo.js
+++ react/todo/src/components/AddTodo.js
@@ -25,11 +25,11 @@ export class AddTodo extends Component {
                 type="text"
                 name="title"
                 style={{ flex: '10', padding: '5px'}}
-                placeholder="Add Todo" 
+                placeholder="Add Todo"
                 value={this.state.title}
                 onChange={this.onChange}
             />
-            <input 
+            <input
                 type="submit" value="Submit" className="btn" style={{flex: '1'}}/>
         </form>
         );
--- react/todo/src/components/Todos.js
+++ react/todo/src/components/Todos.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import TodoItem from './TodoItem';
-import PropTypes from 'prop-types';
+import React from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 class Todos extends React.Component {
-
-    render() {  
-        // console.log(this.props.todos)
-        return this.props.todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} deleteTodo={this.props.deleteTodo}/>
-        ));
-    }
+  render() {
+    // console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        deleteTodo={this.props.deleteTodo}
+      />
+    ));
+  }
 }
 
-// prop types of todos is an array 
+// prop types of todos is an array
 Todos.propTypes = {
-    todos: PropTypes.array.isRequired,
-    markComplete: PropTypes.func.isRequired,
-    deleteTodo: PropTypes.func.isRequired,
-}
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
 
 export default Todos;
