99fa1c1d9d1db0df0d5223e5a986e600a4f4d0eb
enpuyou <youe2@allegheny.edu>
2020-07-07 03:49:37 -0400

    Finish breaking bad project

--- react/breakingbad/src/App.js
+++ react/breakingbad/src/App.js
@@ -3,26 +3,29 @@ import axios from 'axios'
 import './App.css';
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 
 function App() {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState(true)
 
   useEffect(() => {
     const fetchItems = async () => {
       // axios return a promise
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       console.log(result.data)
       setItems(result.data)
       setIsLoading(false)
     }
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
--- react/breakingbad/src/components/characters/CharacterGrid.js
+++ react/breakingbad/src/components/characters/CharacterGrid.js
@@ -1,12 +1,14 @@
 import React from "react";
+import CharacterItem from "./CharacterItem"
+import Spinner from "../ui/Spinner"
 
 const CharacterGrid = ({ isLoading, items }) => {
   return isLoading ? (
-    <h1>Loading</h1>
+    <Spinner />
   ) : (
     <section className="cards">
       {items.map((item) => (
-        <h1>{item.name}</h1>
+        <CharacterItem key={item.character_id} item={item}></CharacterItem>
       ))}
     </section>
   );
